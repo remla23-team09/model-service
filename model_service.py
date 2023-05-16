c75db9c2e1e22bcc28dd858ee313f226403e700a
Krzysztof Garbowicz <kgar@netcompany.com>
2023-05-16 22:34:10 +0200

    use port 8080

--- model_service.py
+++ model_service.py
@@ -107,4 +107,4 @@ def metrics():
     return Response(m, mimetype="text/plain")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
