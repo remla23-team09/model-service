11d2f9f83d4205fb3a790bbad9a281e01180005f
rieschel <emilia.rieschel@live.com>
2023-05-16 18:54:47 +0200

    Added metrics

--- model_service.py
+++ model_service.py
@@ -3,33 +3,24 @@ Flask API of the REMLA base_project model.
 """
 #import traceback
 import joblib
-import re
+#import re
 from flask import Flask, jsonify, request, Response
 from flasgger import Swagger
-import pandas as pd
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+#import pandas as pd
+#from nltk.corpus import stopwords
+#from nltk.stem.porter import PorterStemmer
 import pickle
 import time
-from prometheus_client import start_http_server, Histogram, Summary
+import sys
+from prometheus_client import Counter, Gauge, Histogram, generate_latest
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
-elapsedPredictionTime = 0
-countHappyPredictions = 0
-countSadPredictions = 0
-
-request_latency_histogram = Histogram(
-    'elapsedTime',
-    'Latency of web app requests',
-    ['endpoint']
-)
-
-elapsed_prediction_summary = Summary(
-    'elapsedPredictionTime',
-    'Elapsed time during prediction'
-)
+happy_predictions = Counter("counter_happy_predictions", "Count the number of happy faces.")
+sad_predictions = Counter("counter_sad_predictions", "Count the number of sad faces.")
+prediction_time = Gauge("gauge_prediction_time", "Count the duration for the prediction for different steps.", ["step"])
+size_of_input = Histogram("histogram_size_of_input", "The number of characters in the input.", buckets=[0, 5, 10, 15, 25, 50, 75, 100])
 
 def prepare(text):
     cv = pickle.load(open('c1_BoW_Sentiment_Model.pkl', "rb"))
@@ -60,52 +51,47 @@ def predict():
         description: "The result of the classification: '0' or '1'."
     """
 
-    global countHappyPredictions, countSadPredictions, elapsedPredictionTime, request_latency_histogram, elapsed_prediction_summary
+    global happy_predictions, sad_predictions, prediction_time, size_of_input
 
-    with request_latency_histogram.labels(endpoint='/processing').time():
-        startTime = time.time()
-        input_data = request.get_json()
-        text = input_data.get('text')
-        processed_text = prepare(text)
+    # Process data
+    start_time_processing = time.time()
+    input_data = request.get_json()
+    text = input_data.get('text')
+    processed_text = prepare(text)
+    end_time_processing = time.time()
 
-    with request_latency_histogram.labels(endpoint='/loading').time(): 
-        model = joblib.load('c2_Classifier_Sentiment_Model.joblib')
-
-    with request_latency_histogram.labels(endpoint='/prediction').time():
-        prediction = model.predict(processed_text)[0]
-        endTime = time.time()
+    # Load model and predict
+    start_time_prediction = time.time()
+    model = joblib.load('c2_Classifier_Sentiment_Model.joblib')
+    prediction = model.predict(processed_text)[0]
+    end_time_prediction = time.time()
     
     res = {
         "prediction": str(prediction),
         "text": text
     }
 
+    # Update counters
     if prediction == 1:
-        countHappyPredictions += 1
+        happy_predictions.inc()
     else:
-        countSadPredictions += 1
-    elapsedPredictionTime = endTime - startTime
-    elapsed_prediction_summary.observe(elapsedPredictionTime)
+        sad_predictions.inc()
+    
+    # Update gauge
+    elapsed_time_processing= end_time_processing - start_time_processing
+    prediction_time.labels("processing").set(elapsed_time_processing)
+    elapsed_time_prediction= end_time_prediction - start_time_prediction
+    prediction_time.labels("prediction").set(elapsed_time_prediction)
+
+    # Update histogram
+    size_of_input.observe(len(text))
 
     return jsonify(res)
 
 @app.route('/metrics', methods=['GET'])
 def metrics():
-    #global countHappyPredictions, countSadPredictions, elapsedPredictionTime, request_latency_histogram, elapsed_prediction_summary
-    
-    m = "# HELP num_predictions Number of predictions\n"
-    m += "# TYPE num_predictions counter\n"
-    m += "# HELP num_happy_predictions Number of happy predictions\n"
-    m += "# TYPE num_happy_predictions counter\n"
-    m += "# HELP num_sad_predictions Number of sad predictions\n"
-    m += "# TYPE num_sad_predictions counter\n"
 
-    #m+= "num_predictions{{page=\"sub\"}} {}\n".format(countPredictions)
-    m+= "num_happy_predictions{{page=\"sub\"}} {}\n".format(countHappyPredictions)
-    m+= "num_sad_predictions{{page=\"sub\"}} {}\n".format(countSadPredictions)
-    m+= "elapsed_time{{page=\"sub\"}} {}\n".format(elapsedPredictionTime)
-
-    return Response(m, mimetype="text/plain")
+    return Response(generate_latest(), mimetype="text/plain")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8081, debug=True)
